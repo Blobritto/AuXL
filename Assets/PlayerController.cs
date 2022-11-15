2a0eddb626be48cbaf287e09aff03d4106dda076
Tom Laughlin <tomalaughlin@gmail.com>
2022-11-15 20:26:16 +0000

    First attempt at FSM
    Trying to implement a Finite State Machine into my project, doing this early to get it into the barebones of the project, still need to work out a few kinks however.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,51 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+
+public class PlayerStates
 {
-    //Animator animator;
-    Rigidbody2D rb;
-    SpriteRenderer renderer;
+    public Rigidbody2D rb;
+    public SpriteRenderer renderer;
 
-    [SerializeField] private float _walkSpeed = 10.0f;
-    [SerializeField] private float _jumpHeight = 30.0f;
-    [SerializeField] Transform groundCheck;
-    [SerializeField] Transform groundCheckL;
-    [SerializeField] Transform groundCheckR;
-
-
-    bool isGrounded;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public float _walkSpeed = 10.0f;
+    public float _jumpHeight = 30.0f;
+    public float _airSpeed = 10.0f;
+    public Transform groundCheck;
+    public Transform groundCheckL;
+    public Transform groundCheckR;
+    // common base class for sharing stuff (e.g. static counter variables)
+    // also forces people to implement minimal functionality
+    public virtual void handleInput(PlayerController thisObject) { }
+    public void Awake()
     {
-
-        rb = GetComponent<Rigidbody2D>();
-        renderer = GetComponent<SpriteRenderer>();
-        
+        rb = GameObject.GetComponent<Rigidbody2D>();
+        renderer = GameObject.GetComponent<SpriteRenderer>();
+        currentState = new RunningState();
     }
+};
 
-    // Update is called once per frame
-    void Update()
-    { 
-    }
+public class RunningState : PlayerStates
+{
 
-    private void FixedUpdate()
+    public override void handleInput(PlayerController thisObject)
     {
-        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Floor")) ||
-            Physics2D.Linecast(transform.position, groundCheckL.position, 1 << LayerMask.NameToLayer("Floor")) ||
-            Physics2D.Linecast(transform.position, groundCheckR.position, 1 << LayerMask.NameToLayer("Floor")))
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
-        }
-       
-
-
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
             _walkSpeed = 10.0f;
@@ -61,14 +44,84 @@ public class PlayerController : MonoBehaviour
         {
             _walkSpeed = 0.0f;
         }
-        if (Input.GetKey("space") && isGrounded)
+        if (Input.GetKey("space"))
         {
             rb.velocity = new Vector2(rb.velocity.x, _jumpHeight);
+            thisObject.currentState = new JumpState();
         }
-        
-        
-        
-        rb.velocity = new Vector2(_walkSpeed, rb.velocity.y);
 
+        rb.velocity = new Vector2(_walkSpeed, rb.velocity.y);
+    }
+}
+
+public class JumpState : PlayerStates
+{
+    public GameObject player1;
+
+    public override void handleInput(PlayerController thisObject)
+    {
+        while (Physics2D.Linecast(thisObject.transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Floor")) ||
+        Physics2D.Linecast(thisObject.transform.position, groundCheckL.position, 1 << LayerMask.NameToLayer("Floor")) ||
+        Physics2D.Linecast(thisObject.transform.position, groundCheckR.position, 1 << LayerMask.NameToLayer("Floor")))
+        {
+            if (Input.GetKey("d") || Input.GetKey("right"))
+            {
+                _airSpeed = 10.0f;
+                renderer.flipX = false;
+            }
+            else if (Input.GetKey("a") || Input.GetKey("left"))
+            {
+                _airSpeed = -10.0f;
+                renderer.flipX = true;
+            }
+        }
+
+        rb.velocity = new Vector2(_airSpeed, rb.velocity.y);
+        thisObject.currentState = new RunningState();
+    }
+}
+
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    //Animator animator;
+    /*    Rigidbody2D rb;
+        SpriteRenderer renderer;
+
+        public float _walkSpeed = 10.0f;
+        public float _jumpHeight = 30.0f;
+        public float _airSpeed = 10.0f;
+        public Transform groundCheck;
+        public Transform groundCheckL;
+        public Transform groundCheckR;*/
+
+
+    public PlayerStates currentState;
+
+
+    bool isGrounded;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        /* rb = GetComponent<Rigidbody2D>();
+         renderer = GetComponent<SpriteRenderer>();
+         currentState = new RunningState();*/
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        currentState.Awake();
+        currentState.handleInput(this);
     }
 }
