16b2ed271ff26c7e6daaa2ecb81010b37e9a3f95
Tom Laughlin <tomalaughlin@gmail.com>
2022-11-26 17:53:33 +0000

    New level, tutorial finished

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -6,15 +6,18 @@ public class Button : MonoBehaviour
 {
     public Rigidbody2D rb;
     public SpriteRenderer renderer;
-    public GameObject door;
+    public GameObject[] door;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         renderer = GetComponent<SpriteRenderer>();
         renderer.color = new Color(1, 0, 0, 1);
-        door.SetActive(true);
-        
+        for (int i = 0; i < (door.Length - 1); i++)
+        {
+            door[i].SetActive(true);
+        }
+
     }
 
     // Update is called once per frame
@@ -30,12 +33,18 @@ public class Button : MonoBehaviour
             if (renderer.color == new Color(1, 0, 0, 1))
             {
                 renderer.color = new Color(0, 1, 0, 1);
-                door.SetActive(false);
+                for (int i = 0; i < (door.Length - 1); i++)
+                {
+                    door[i].SetActive(false);
+                }
             }  
             else
             {
                 renderer.color = new Color(1, 0, 0, 1);
-                door.SetActive(true);
+                for (int i = 0; i < (door.Length - 1); i++)
+                {
+                    door[i].SetActive(true);
+                }
             }
         }
     }
