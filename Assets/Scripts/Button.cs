6c4fc3c814d47835fbdb1e0677d9fe2ff6dea86c
Tom Laughlin <tomalaughlin@gmail.com>
2022-11-26 23:58:06 +0000

    Level 13 complete
     make 4 or 5 more levels showing off all the parts of the game

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -7,19 +7,32 @@ public class Button : MonoBehaviour
     public Rigidbody2D rb;
     public SpriteRenderer renderer;
     public GameObject[] door;
+    public GameObject[] doorClose;
+    public GameObject[] permaLock;
+    Color temp;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         renderer = GetComponent<SpriteRenderer>();
         renderer.color = new Color(1, 0, 0, 1);
-        DoorSet(true);
+        temp = permaLock[0].GetComponent<SpriteRenderer>().color;
+        DoorSet(true, door);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey("r"))
+        {
+            renderer.color = new Color(1, 0, 0, 1);
+            DoorSet(true, door);
+            for (int i = 0; i <= (permaLock.Length - 1); i++)
+            {
+                permaLock[i].GetComponent<SpriteRenderer>().color = temp;
+            }
+        }
         
     }
 
@@ -30,17 +43,29 @@ public class Button : MonoBehaviour
             if (renderer.color == new Color(1, 0, 0, 1))
             {
                 renderer.color = new Color(0, 1, 0, 1);
-                DoorSet(false);
+                DoorSet(false, door);
+                DoorSet(true, doorClose);
+                DoorSet(true, permaLock);
+                for (int i = 0; i <= (permaLock.Length - 1); i++)
+                {
+                    permaLock[i].GetComponent<SpriteRenderer>().color = new Color(1, 0, 1, 1);
+                }
             }  
             else
             {
                 renderer.color = new Color(1, 0, 0, 1);
-                DoorSet(true);
+                DoorSet(true, door);
+                DoorSet(false, doorClose);
+                DoorSet(true, permaLock);
+                for (int i = 0; i <= (permaLock.Length - 1); i++)
+                {
+                    permaLock[i].GetComponent<SpriteRenderer>().color = new Color(1, 0, 1, 1);
+                }
             }
         }
     }
 
-    void DoorSet(bool active)
+    void DoorSet(bool active, GameObject[] door)
     {
         for (int i = 0; i <= (door.Length - 1); i++)
         {
