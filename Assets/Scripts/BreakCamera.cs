5ee324b903678f28f6e6159e25848cb94e123c35
Tom Laughlin <tomalaughlin@gmail.com>
2022-11-27 20:34:41 +0000

    Comments and level tweaks

--- Assets/Scripts/BreakCamera.cs
+++ Assets/Scripts/BreakCamera.cs
@@ -5,27 +5,22 @@ using UnityEngine;
 public class BreakCamera : MonoBehaviour
 {
     Rigidbody2D rb;
-
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        // The camera floats by default.
         rb.gravityScale = 0f;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
     void OnCollisionEnter2D(Collision2D col)
     {
+        // When the camera breaks, it falls to the ground, and the beam is destroyed.
         if (col.gameObject.tag == "Coin" || col.gameObject.tag == "Player")
         {
             rb.gravityScale = 3f;
             GameObject.Destroy(transform.GetChild(0).gameObject);
         }
-
+        // When it hits the floor, it destroys itself.
         if (col.gameObject.tag == "Floor" && transform.childCount == 0)
         {
             GameObject.Destroy(gameObject);
